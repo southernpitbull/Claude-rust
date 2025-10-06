b48bf2efa12948f069d389adea6cc4711f8389cf
southernpitbull <77404848+southernpitbull@users.noreply.github.com>
2025-10-06 16:14:46 -0400

    Delete core directory

--- core/src/codebase/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod analyzer;
-pub mod scanner;
-
-pub use analyzer::{AnalysisResult, CodeAnalyzer, CodeMetrics, Dependency, DependencyKind, Function, Symbol, SymbolKind, Class};
-pub use scanner::{CodeFile, CodebaseError, CodebaseResult, CodebaseScanner, CodebaseStatistics, ScannerConfig};
